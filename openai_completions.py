17e09d17f16db94ed731debd2a7ebe6ef664f4af
hare <mike@mikehare.io>
2022-11-04 13:10:06 -0400

    Revised .gitignore.

--- openai_completions.py
+++ openai_completions.py
@@ -1,17 +1,21 @@
 ##################################################
-##  OPENAI GPT-3 API: Completions
+####  OPENAI GPT-3 API: COMPLETIONS
 ##################################################
 
 import requests
-import config
+import Config
 import openai
 
-API_KEY = config.api_key
+API_KEY = Config.api_key
 
 openai.api_key = API_KEY
 
 prompt = "Write a tagline for an ice cream shop."
 
-response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
+response = openai.Completion.create(
+    engine = "text-davinci-001", 
+    prompt = prompt, 
+    max_tokens = 10
+)
 
 print(response)
